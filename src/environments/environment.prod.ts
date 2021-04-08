4a7a52a5cc5436d8f675f32245c19d1f5e1bd24d
gregnaninl <gregnanin.leandro@gmail.com>
2021-04-08 15:18:59 -0300

    login2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyD2hrYkEQ44wFHNaBdEXwfhkHwBRWIg4P0",
+    authDomain: "juegos-gregnanin.firebaseapp.com",
+    databaseURL: "https://juegos-gregnanin-default-rtdb.firebaseio.com",
+    projectId: "juegos-gregnanin",
+    storageBucket: "juegos-gregnanin.appspot.com",
+    messagingSenderId: "307993391676",
+    appId: "1:307993391676:web:7834da461deb92d30fc939",
+    measurementId: "G-64ZRRLX323"
+  }
 };
